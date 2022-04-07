d66fae706afeb506480b8fdd040f624784917ab5
ionutmihaiyt <37551208+ionutmihaiyt@users.noreply.github.com>
2022-04-07 20:46:21 +0300

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -28,7 +28,7 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "lavalink.devz.cloud",
+    host: "usui-linku.kadantte.moe",
     port: 443, // The port that lavalink is listening to. This must be a number!
     pass: "Usui#0256",
     secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
